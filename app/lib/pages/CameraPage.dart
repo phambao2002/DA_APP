8c934df6ba6928241aa9ed9ecd9428f3e9f46901
trieuquangbinh53837 <117162642+trieuquangbinh53837@users.noreply.github.com>
2024-05-20 03:20:53 +0700

    update camera

--- app/lib/pages/CameraPage.dart
+++ app/lib/pages/CameraPage.dart
@@ -1,25 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:app/config/themes/theme.dart';
-import 'package:app/services/disease_provider.dart';
-import 'package:app/src/home_page/models/disease_model.dart';
-import 'package:app/src/suggestions_page/suggestions.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:app/services/classify.dart';
-import 'package:app/services/hive_database.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:app/pages/accountPage.dart';
-import 'package:app/pages/plantPage.dart';
-import 'package:app/src/home_page/components/greeting.dart';
-import 'package:app/src/home_page/components/history.dart';
-import 'package:flutter/widgets.dart';
-import 'package:app/src/home_page/components/titlesection.dart';
-import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite_v2/tflite_v2.dart';
+import 'dart:io';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CameraPage extends StatefulWidget {
   @override
@@ -32,37 +16,38 @@ class _CameraPageState extends State<CameraPage> {
   File? file;
   var _recognitions;
   var v = "";
-  // var dataList = [];
+
   @override
   void initState() {
     super.initState();
-    loadmodel().then((value) {
+    loadModel().then((value) {
       setState(() {});
     });
   }
 
-  loadmodel() async {
+  Future<void> loadModel() async {
     await Tflite.loadModel(
       model: "assets/model_unquant.tflite",
       labels: "assets/labels.txt",
     );
   }
 
-  Future<void> _pickImage() async {
+  Future<void> _pickImage(ImageSource source) async {
     try {
-      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      final XFile? image = await _picker.pickImage(source: source);
       setState(() {
         _image = image;
-        file = File(image!.path);
+        if (image != null) {
+          file = File(image.path);
+          detectImage(file!);
+        }
       });
-      detectimage(file!);
     } catch (e) {
       print('Error picking image: $e');
     }
   }
 
-  Future detectimage(File image) async {
-    int startTime = new DateTime.now().millisecondsSinceEpoch;
+  Future<void> detectImage(File image) async {
     var recognitions = await Tflite.runModelOnImage(
       path: image.path,
       numResults: 6,
@@ -73,14 +58,10 @@ class _CameraPageState extends State<CameraPage> {
     setState(() {
       _recognitions = recognitions;
       v = recognitions.toString();
-      // dataList = List<Map<String, dynamic>>.from(jsonDecode(v));
     });
     print("//////////////////////////////////////////////////");
     print(_recognitions);
-    // print(dataList);
     print("//////////////////////////////////////////////////");
-    int endTime = new DateTime.now().millisecondsSinceEpoch;
-    print("Inference took ${endTime - startTime}ms");
   }
 
   @override
@@ -103,14 +84,35 @@ class _CameraPageState extends State<CameraPage> {
             else
               Text('No image selected'),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _pickImage,
-              child: Text('Pick Image from Gallery'),
-            ),
-            SizedBox(height: 20),
+            // ElevatedButton(
+            //   onPressed: () => _pickImage(ImageSource.gallery),
+            //   child: Text('Gallery'),
+            // ),
+            // SizedBox(height: 20),
             Text(v),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: SpeedDial(
+        icon: Icons.camera_alt,
+        spacing: 10,
+        children: [
+          SpeedDialChild(
+            child: const FaIcon(
+              Icons.image_search,
+            ),
+            label: "Gallery",
+            onTap: () => _pickImage(ImageSource.gallery),
+          ),
+          SpeedDialChild(
+            child: const FaIcon(
+              FontAwesomeIcons.camera,
+            ),
+            label: "Camera",
+            onTap: () => _pickImage(ImageSource.camera),
+          ),
+        ],
       ),
     );
   }
